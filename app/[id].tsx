d9cb30fe2cafc7faf73cfd68f9920d0c509b4238
Tibamwenda Anthony <anthonyngisiro@gmail.com>
2024-09-17 17:43:41 +0300

    RSVP footer

--- app/[id].tsx
+++ app/[id].tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import events from '~/assets/events.json';
 import dayjs from 'dayjs';
+import AdaptiveButton from '~/components/AdaptiveButton';
 
 export default function EventPage() {
   const { id } = useLocalSearchParams();
@@ -13,16 +14,24 @@ export default function EventPage() {
   }
 
   return (
-    <View className="flex-1 bg-white p-3">
+    <View className="flex-1 justify-between bg-white p-3">
       <Stack.Screen options={{ headerTitle: 'Event', headerTitleAlign: 'center' }} />
-      <Image source={{ uri: event.image }} className="aspect-video w-full rounded-xl" />
-      <Text className="text-lg font-bold" numberOfLines={2}>
-        {event.name}
-      </Text>
-      <Text className="text-blue-800">
-        {dayjs(event.datetime).format('ddd,  MMM')} · {dayjs(event.datetime).format('h:mm A')}
-      </Text>
-      <Text className="my-2 text-lg leading-tight">{event.description}</Text>
+      <View className="">
+        <Image source={{ uri: event.image }} className="aspect-video w-full rounded-xl" />
+        <Text className="text-lg font-bold" numberOfLines={2}>
+          {event.name}
+        </Text>
+        <Text className="text-blue-800">
+          {dayjs(event.datetime).format('ddd,  MMM')} · {dayjs(event.datetime).format('h:mm A')}
+        </Text>
+        <Text className="my-2 text-lg leading-tight">{event.description}</Text>
+      </View>
+
+      {/* Footer */}
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t border-gray-200 p-3">
+        <Text className="text-lg font-semibold">Free</Text>
+        <AdaptiveButton className="w-1/2" size="large" title="Join and RSVP" />
+      </View>
     </View>
   );
 }
